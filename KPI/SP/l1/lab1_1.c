7cd92294af71e9f63052c9b869a3cf07adcc8c22
AChepurnoi <sasha.chepurnoii@gmail.com>
2017-03-05 02:04:52 +0200

    SP lab 1

---
+++ KPI/SP/l1/lab1_1.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+extern int errno ;
+
+void handleError(){
+	printf("%s\n", strerror(errno) );
+	exit(0);
+}
+
+void toLowerCase(char *string, size_t size){
+	for(int i = 0; i < size; i++){
+		char l = string[i];
+		if (l > 64 && l < 91) string[i] = l + 32;
+	}
+}
+
+int main(int argc, char *argv[]){
+
+	int BUFFER_SIZE = 500;
+
+	if(argc != 3){
+		printf("Wrong params\n");
+		return -1;
+	}
+
+	int source = open(argv[1],O_RDONLY);
+	if(source < 0) handleError();
+
+	int dest = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if(dest < 0) handleError();
+
+	char *buffer = malloc(BUFFER_SIZE);
+	int total = 0;
+
+	while(1) {
+		int readed = read(source, buffer, BUFFER_SIZE);
+		if (readed == 0) break;
+		if (readed == -1) handleError();
+		toLowerCase(buffer, readed);
+		int written = write(dest, buffer, readed);
+		//written < readed?
+		if(written == -1 )handleError();
+		total = total + readed;
+	}
+	free(buffer);
+	printf("%d Total bytes\n",total);
+}
+
---
+++ KPI/SP/l1/lab1_2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+extern int errno ;
+
+void handleError(){
+	printf("%s\n", strerror(errno) );
+	exit(0);
+}
+
+
+int main(int argc, char ** argv){
+
+	if( argc != 2){
+		printf("Wrong input\n");
+		exit(-1);
+	}
+
+	size_t MAXSIZE = 1024;
+	fd_set rfds;
+	struct timeval tv;
+	/* Wait up to five seconds. */
+	tv.tv_sec = 5;
+	tv.tv_usec = 0;
+
+	int retval = 0;
+	while(!retval){
+		printf("Waiting input within five seconds\n");
+		FD_ZERO(&rfds);
+		FD_SET(STDIN_FILENO, &rfds);
+		retval = select(STDIN_FILENO + 1, &rfds, NULL, NULL, &tv);
+		
+		if (retval == -1) handleError();
+		else if (retval){
+			char *buffer = malloc(MAXSIZE);
+			int result = read(STDIN_FILENO, buffer, MAXSIZE);
+			if (result == -1) handleError();
+		    printf("%s: %s\n", argv[1], buffer);
+		    free(buffer);
+		}
+	}
+
+
+}
