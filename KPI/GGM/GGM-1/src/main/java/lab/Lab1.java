14c304513f5c9330f0777dd1bc2fbd02731a4648
AChepurnoi <sasha.chepurnoii@gmail.com>
2017-03-07 02:14:49 +0200

    GGM 1/2

---
+++ KPI/GGM/GGM-1/src/main/java/lab/Lab1.java
@@ -0,0 +1,33 @@
+package lab;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+/**
+ * Created by Sasha on 3/5/17.
+ */
+
+public class Lab1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Lab 1");
+        final Group root = new Group();
+        final Scene scene = new Scene(root, 800, 600);
+        scene.setFill(Color.ORANGE);
+
+        Pram pram = new Pram(275,175);
+        root.getChildren().addAll(pram.drawableParts());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
