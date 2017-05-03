0a3b2657adb64e3a1ffd3ab5c3ff7b5e1acbbd8e
AChepurnoi <sasha.chepurnoii@gmail.com>
2017-05-03 21:05:48 +0300

    Aiteken

--- KPI/NM/RGR1/Task1/PowerIterationAiteken.m
+++ KPI/NM/RGR1/Task1/PowerIterationAiteken.m
@@ -1,19 +1,24 @@
-function [ eig, eigv ] = PowerIteration(A)
+function [ eig, eigv ] = PowerIterationAiteken(A)
+matrix_size = size(A,1);
 epsilon = 0.001;
 r = 2;
 p = 3;
+q = 3;
 sigma = 0.001;
-y_init = [1 1 1];
+y_init = ones(1, matrix_size);
 z = (y_init / norm(y_init))';
 B = A ^ (2 ^ r);
-S_prev = [1 2 3];
+S_prev = 1:matrix_size;
 z_k = z;
-
-lambda_k = [100 100 100]';
+z_k_prev = z_k;
+lambda_k = ones(matrix_size, 1) .* 100;
+lambda_k_prev = ones(matrix_size, 1) .* 100;
 
 for k = 1:inf
+    z_k_prev_prev = z_k_prev;
     z_k_prev = z_k;
     y_k = B * z_k_prev;
+    lambda_k_prev_prev = lambda_k_prev;
     lambda_k_prev = lambda_k;
     lambda_k = y_k ./ z_k_prev;
     
@@ -27,6 +32,11 @@ for k = 1:inf
        z_k = y_k;
     end
     
+    if mod(k, q) == 0
+        z_k = z_k_prev_prev - ((z_k_prev - z_k_prev_prev).^2 ./ ...
+            (z_k - 2 * z_k_prev + z_k_prev_prev));
+    end
+    
     intersection = intersect(S_prev, S_prev_prev)';
     
     shouldStop = PowerMethodStopCriteria(lambda_k, lambda_k_prev, intersection, epsilon);
@@ -36,11 +46,12 @@ for k = 1:inf
     end
 end
 
+
 y_k_next = A * z_k;
 lambda_next = y_k_next ./ z_k;
-S = find(abs(z_k) > sigma);
-eig = sum(lambda_next) / norm(S);
-eigv = y_k_next / norm(y_k_next);
 
+S = find(abs(z_k) > sigma);
+eig = sum(lambda_next(S)) / size(S,1);
+eigv = y_k_next / norm(y_k_next);
 end
 
--- KPI/NM/RGR1/Task1/main.m
+++ KPI/NM/RGR1/Task1/main.m
@@ -1,10 +1,10 @@
 %  M = [ 1 2 3; 4 5 6; 7 8 9];
-% M = [1 0 0; 0 2 1; 1 0 1];
+M = [1 0 0; 0 2 1; 1 0 1];
 
  
 % M = [-2 -2 3 ; -10 -1 6; 10 -2 -9];
 
-M = [
+ M = [
      -691 -585 593 212 733; 
      -331 -308 342 -82 138;
      56 -79 -778 27 267;
@@ -14,17 +14,19 @@ M = [
 
 % M = [5 1 2; 1 4 1; 2 1 3];
 [eigvalue, eigvector] = PowerIteration(M);
+% M = [2 -1 1; -1 2 -1; 0 0 3];
+[aeigvalue, aeigvector] = PowerIterationAiteken(M);
 
-[seigval, seigvec] = PowerIterationSecondPair(M, eigvalue, eigvector, 0.001);
+% [seigval, seigvec] = PowerIterationSecondPair(M, eigvalue, eigvector, 0.001);
 % M is negative defined matrix, so do the shift
-B = M - eye(5) * eigvalue;
+% B = M - eye(5) * eigvalue;
 
-[smallval, smallvec] = PowerIteration(B);
-[smallseigval, smallseigvec] = PowerIterationSecondPair(B, smallval, smallvec, 0.001);
+% [smallval, smallvec] = PowerIteration(B);
+% [smallseigval, smallseigvec] = PowerIterationSecondPair(B, smallval, smallvec, 0.001);
 
 % Adding Max eigval to results to get two smalles eigvals
-smallval = smallval + eigvalue;
-smallseigval = smallseigval + eigvalue;
+% smallval = smallval + eigvalue;
+% smallseigval = smallseigval + eigvalue;
 
 
 [V D W] = eig(M);
