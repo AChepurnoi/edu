97ed5ba04ea7c3665a1fd4a8aca9f0d307b19b8d
AChepurnoi <sasha.chepurnoii@gmail.com>
2017-05-21 23:54:19 +0300

    Fixed constants for ubuntu

--- KPI/SP/l5/constants.c
+++ KPI/SP/l5/constants.c
@@ -1,3 +1,9 @@
+
+#define L3HEADERSIZE_NUM 40
+#define L2HEADERSIZE_NUM 17
+#define MSS_NUM 60
+#define L2SUFFIX_NUM 3
+
 const char *fileparam = "-f";
 const char *pipeparam = "-p";
 const char *recieveModeParam = "-r";
@@ -10,12 +16,12 @@ const int BUFFER_MAX_SIZE = 2000;
 const int recieveMode = 1;
 const int transmitMode = 0;
 
-const int MSS = 60;
-const int L3HEADERSIZE = 40;
-const int L2HEADERSIZE = 17;
-const int L2SUFFIX = 3;
-const int L3PDUSIZE = L3HEADERSIZE + MSS;
-const int L2PDUSIZE = L2HEADERSIZE + L3PDUSIZE + L2SUFFIX;
+const int MSS = MSS_NUM;
+const int L3HEADERSIZE = L3HEADERSIZE_NUM;
+const int L2HEADERSIZE = L2HEADERSIZE_NUM;
+const int L2SUFFIX = L2SUFFIX_NUM;
+const int L3PDUSIZE = L3HEADERSIZE_NUM + MSS_NUM;
+const int L2PDUSIZE = L2HEADERSIZE_NUM + L3HEADERSIZE_NUM + MSS_NUM + L2SUFFIX_NUM;
 const int MAXPDU = 40;
 
 const int RESPONSESIZE = 4;
